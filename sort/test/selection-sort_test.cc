4d1cd4482f4c7e71db8ddcae9a3479e0e19e7071
Vicente Romero <vteromero@gmail.com>
2013-08-18 18:38:36 +0200

    improved test programs of sorting algorithms

--- sort/test/selection-sort_test.cc
+++ sort/test/selection-sort_test.cc
@@ -14,19 +14,29 @@
 
 int main()
 {
-    SelectionSort ssort;
-    int array[] = {22, 4, 3, 7, 11, 34, 9, 1, 6, 2, 2, 66, 6, 4, 88, 83};
+    int array[] = {
+        -2569, 17, 1, -5789, 986, 42, -170, -87, -136, -20,
+        428, 15, 64, 8983, 1424, -627, 53, -13, 95, 20,
+        -37, 1086, 136, -322, 179, 38, -1, 16, -135, 975,
+        -103, -5045, 10, 1722, 16, 406, 591, -879, 33, 6,
+        -867, 99, -139, -166, -15, -199, -3952, -1734, 174, 217
+    };
     int length = NELEMS(array);
 
+    printf("\nInitial array:\n");
+    PrintArray(array, length);
+
+    SelectionSort ssort;
     ssort.Sort(array, length);
+
+    printf("\nSorted array:\n");
+    PrintArray(array, length);
 
     bool sorted = CheckSortedArray(array, length, ASC_SORT);
     if(sorted)
-        printf("Checking array... The array is sorted.\n");
+        printf("\nChecking array... The array is sorted.\n");
     else
-        printf("Checking array... The array is NOT sorted.\n");
-
-    PrintArray(array, length);
+        printf("\nChecking array... The array is NOT sorted.\n");
 
     SortStats *stats = ssort.Statistics();
     if(stats != NULL)
